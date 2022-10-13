41b235aa0e08c7865064d36b7016e2040e65e035
chukwujike <chukwujike2000@gmail.com>
2022-10-13 14:17:35 -0700

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,13 @@
 
 #include <unistd.h>
 
-/**	
- 	* main - prints exactly and that piece of art is useful
-	*
+/**
+	* main - prints exactly and that piece of art is useful
 	* Dora Korpa,
 	*
 	* followed by a new line, to the standard error.
 	*
 	* Return Always (Success)
-	*
 	*
 	*/
 
