0574fec14980f2566efc2b899e6875dc6ad27c35
chukwujike <chukwujike2000@gmail.com>
2022-10-16 14:04:38 -0700

    first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+	*
+	* main - prints the alphabet in lowercase, except q and e
+	*
+	* followed by a new line
+	*
+	* Return: 0 on Success
+	*
+	*/
+
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			continue;
+		}
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+	*
+	* main - prints all single digit base 10 number
+	*
+	* Return: 0 on succes
+	*
+	*/
+
+int main(void)
+{
+	int n;
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
