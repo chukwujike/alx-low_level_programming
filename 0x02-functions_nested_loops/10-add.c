437ecc18e96e98c066a778e5bd3837c1115d8766
chukwujike <chukwujike2000@gmail.com>
2022-10-18 17:07:22 -0700

    first commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+	* add - adds two integers
+	*
+	* @n1: integer to be added
+	*
+	* @n2: integer to be added
+	*
+	* Return: sum of two integers
+	*
+	*/
+
+int add(int n1, int n2)
+{
+	int sum = n1 + n2;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+	* print_to_98 - prints all natural numbers from n to 98
+	*
+	* @n: number to start count
+	*
+	*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+				printf("%d\n", i);
+			else if (i != 98)
+				printf("%d, ", i);
+		}
+	}
+	else if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+				printf("%d\n", i);
+			else if (i != 98)
+				printf("%d, ", i);
+		}
+	}
+}
